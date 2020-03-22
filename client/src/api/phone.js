a9ac099e65d1eeeb015c17c97cb9ea88ab663f34
asdy <adoptdontbuy2019@gmail.com>
2020-03-22 21:46:02 +0100

    Delete unused code

--- client/src/api/phone.js
+++ client/src/api/phone.js
@@ -4,15 +4,10 @@ const endpoint_local = 'http://localhost:3001/phones';
 
 const api = {
     phones: {
-        async read(id = null) {
+        async read() {
             try {
-                if (!id) {
-                    const response = await axios.get(endpoint_local);
-                    return Promise.resolve(response);
-                }
-                const response = await  axios.get(`${endpoint_local}/${id}`);
-                const data = await response.json();
-                return Promise.resolve(data);
+                const response = await axios.get(endpoint_local);
+                return Promise.resolve(response);
             } catch (error) {
                 console.log("Error retrieving the data from API")
                 return Promise.reject(error);
