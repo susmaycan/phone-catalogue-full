4671e2b8d85295ab75e45f1e8732b434e1e8d004
asdy <adoptdontbuy2019@gmail.com>
2020-05-20 10:54:43 +0200

    Add tests to reducer

---
+++ client/src/reducers/phoneReducer.spec.js
@@ -0,0 +1,69 @@
+import reducer from './phoneReducer'
+import {PHONE_LIST, SEARCH_TEXT} from '../Utils/constTest'
+import actionTypes from '../actions/actionTypes'
+
+const INITIAL_STATE = {
+    isLoading: false,
+    error: false,
+    phones: [],
+    searchTerm: ''
+}
+
+describe('Phone reducer', () => {
+    it('should return the initial state', () => {
+        expect(reducer(undefined, {}))
+            .toEqual(INITIAL_STATE)
+    })
+
+    it('should handle set input text', () => {
+        expect(
+            reducer(INITIAL_STATE, {
+                type: actionTypes.SET_FILTER,
+                text: SEARCH_TEXT
+            })
+        ).toEqual({
+                ...INITIAL_STATE,
+                searchTerm: SEARCH_TEXT
+            }
+        )
+    })
+
+    it('should handle request phones', () => {
+        expect(
+            reducer(INITIAL_STATE, {
+                type: actionTypes.FETCH_REQUEST
+            })
+        ).toEqual({
+                ...INITIAL_STATE,
+                isLoading: true
+            }
+        )
+    })
+
+    it('should handle fetch phones successfully', () => {
+        expect(
+            reducer(INITIAL_STATE, {
+                type: actionTypes.FETCH_SUCCESS,
+                payload: PHONE_LIST
+            })
+        ).toEqual({
+                ...INITIAL_STATE,
+                isLoading: false,
+                phones: PHONE_LIST
+            }
+        )
+    })
+
+    it('should handle fetch phones error', () => {
+        expect(
+            reducer(INITIAL_STATE, {
+                type: actionTypes.FETCH_ERROR
+            })
+        ).toEqual({
+                ...INITIAL_STATE,
+                isLoading: false,
+                error: true
+            }
+        )
+    })
+})
