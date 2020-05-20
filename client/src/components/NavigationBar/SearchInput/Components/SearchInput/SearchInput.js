ce6478d257672388b23dc180a2c6b106a28c8e7f
asdy <adoptdontbuy2019@gmail.com>
2020-05-20 23:11:33 +0200

    Change formcontrol

--- client/src/components/NavigationBar/SearchInput/Components/SearchInput/SearchInput.js
+++ client/src/components/NavigationBar/SearchInput/Components/SearchInput/SearchInput.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import {
-    Form,
-    FormControl
+    Form
 } from 'react-bootstrap'
 import PropTypes from 'prop-types'
 
 const SearchInput = ({ setFilter, error }) => (
     <Form inline>
-        <FormControl disabled={error} onChange={event => setFilter(event.target.value)} type="text" placeholder="Search..." className="mr-sm-2" />
+        <input disabled={error} onChange={event => setFilter(event.target.value)} type="text" placeholder="Search..." className="form-control mr-sm-2" />
     </Form>
 )
 
 SearchInput.propTypes = {
     setFilter: PropTypes.func.isRequired,
+    error: PropTypes.bool.isRequired
 }
 
 export default SearchInput
